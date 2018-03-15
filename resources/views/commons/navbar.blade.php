e83001ca4c5e3b3a955a410a57a02ea91053783b
koki.kitao <ec2-user@ip-172-31-8-138.us-east-2.compute.internal>
2018-03-15 12:09:00 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-inverse navbar-static-top">
-        <div class="container-fluid">
+        <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
@@ -12,11 +12,23 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('tasks.create', '新規タスクの作成') !!}</li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                    <li><a href="#">Login</a></li>
+                    @if (Auth::check())
+                        <li><a href="#">Users</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">My profile</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                        </li>
+                        <li>{!! link_to_route('tasks.create', '新規タスクの作成') !!}</li>
+                    @else
+                        <ul class="nav navbar-nav navbar-right">
+                          <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                          <li>{!! link_to_route('login.get', 'Login') !!}</li>
+                        </ul>
+                    @endif
                 </ul>
             </div>
         </div>
