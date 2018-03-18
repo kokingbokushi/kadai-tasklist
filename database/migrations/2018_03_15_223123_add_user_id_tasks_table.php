f0f82afa3c1b1761bfac1e5c023610188fc76a52
koki.kitao <ec2-user@ip-172-31-8-138.us-east-2.compute.internal>
2018-03-18 13:30:40 +0000

    correct AddUserIdTable class

--- database/migrations/2018_03_15_223123_add_user_id_tasks_table.php
+++ database/migrations/2018_03_15_223123_add_user_id_tasks_table.php
@@ -1,33 +1,33 @@
-// <?php
+<?php
 
-// use Illuminate\Database\Schema\Blueprint;
-// use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-// class AddUserIdTasksTable extends Migration
-// {
-//     /**
-//      * Run the migrations.
-//      *
-//      * @return void
-//      */
-//     public function up()
-//     {
-//         Schema::table('tasks', function (Blueprint $table) {
-//             $table->integer('user_id')->unsigned()->index();
+class AddUserIdTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index();
             
-//             $table->foreign('user_id')->references('id')->on('users');
-//         });
-//     }
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
 
-//     /**
-//      * Reverse the migrations.
-//      *
-//      * @return void
-//      */
-//     public function down()
-//     {
-//         Schema::table('tasks', function (Blueprint $table) {
-//             $table->dropColumn('user_id');
-//         });
-//     }
-// }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
